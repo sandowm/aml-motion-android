c9a29f59a8b51d165c7f8c4808a2714cb3cebef6
Malte Sandow <malte.sandow@mobi.ch>
2020-10-31 12:19:42 +0100

    Initial

---
+++ app/src/main/java/com/example/malteactivity/SeismicDataPoint.java
@@ -0,0 +1,12 @@
+package com.example.malteactivity;
+
+public class SeismicDataPoint {
+    public int x;
+    public float y;
+
+    public SeismicDataPoint(int x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
